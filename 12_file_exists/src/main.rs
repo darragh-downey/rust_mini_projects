6c9af18ae26ced3c84398861cffce8fa9b07f4b8
Darragh Downey <darragh.downey90@gmail.com>
2023-03-12 18:29:13 +1100

    finished file exists

--- 12_file_exists/src/main.rs
+++ 12_file_exists/src/main.rs
@@ -1,3 +1,65 @@
+// use libc::access;
+use std::fs;
+use std::path;
+
+trait FileMetadata {
+    fn exists(&self) -> bool;
+
+    fn is_writeable(&self) -> bool;
+
+    fn is_readable(&self) -> bool;
+}
+
+impl FileMetadata for path::Path {
+    fn is_readable(&self) -> bool {
+        fs::File::open(&self).is_ok()
+    }
+
+    fn is_writeable(&self) -> bool {
+        if let Ok(meta) = fs::metadata(self) {
+            !meta.permissions().readonly()
+        } else {
+            false
+        }
+
+        // or
+        // fs::metadata(self)
+        //     .map(|m| {
+        //         !m.permissions().readonly()
+        //     })
+        //     .unwrap_or(false)
+    }
+
+    fn exists(&self) -> bool {
+        self.exists()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    //
+}
+
+#[test]
+fn writeable() {
+    use std::fs;
+    use tempfile;
+
+    let f = tempfile::NamedTempFile::new().unwrap();
+    assert!(f.path().is_writeable());
+
+    fs::remove_file(f.path()).unwrap();
+}
+
+#[test]
+fn read_only() {
+    use std::fs;
+    use tempfile;
+
+    let f = tempfile::NamedTempFile::new().unwrap();
+    let mut perms = fs::metadata(f.path()).unwrap().permissions();
+    perms.set_readonly(true);
+    fs::set_permissions(f.path(), perms).unwrap();
+    assert_eq!(f.path().is_writeable(), false);
+
+    fs::remove_file(f.path()).unwrap();
 }
